c25b59e10338a453d4deedb781a083429a1c8b88
Mohanadurga Gudella <147473112+mohana5141@users.noreply.github.com>
2026-06-30 09:24:38 +0530

    Time: 0 ms (100%), Space: 8.7 MB (93.4%) - LeetHub

---
+++ 3853-merge-close-characters/3853-merge-close-characters.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string mergeCharacters(string s, int k) {
+        while (true) {
+            bool merged = false;
+
+            for (int i = 0; i < s.size() && !merged; i++) {
+                for (int j = i + 1; j < s.size(); j++) {
+                    if (s[i] == s[j] && j - i <= k) {
+                        s.erase(j, 1);
+                        merged = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!merged)
+                break;
+        }
+
+        return s;
+    }
+};
